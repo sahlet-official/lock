f05dc05b4b21d5dd9949cafc29cb49affcb30e78
Sasha Human <sahlet.official@gmail.com>
2025-04-24 11:45:00 +0200

    Update post.ts

--- src/post.ts
+++ src/post.ts
@@ -9,11 +9,7 @@ async function run() {
   const auto_unlock = core.getBooleanInput('auto_unlock');
   const fail_if_cant_unlock = core.getBooleanInput('fail_if_cant_unlock');
 
-  core.info(`FIND ME before`);
-
   const locked = core.getState('locked') === 'true';
-
-  core.info(`FIND ME locked = "${locked}"`);
 
   const octokit = github.getOctokit(token);
   const { repo, owner } = github.context.repo;
